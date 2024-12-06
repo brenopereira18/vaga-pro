6fedc7e0d703534bb6e9d98ddabe73298a973e02
brenopereira18 <brenopereira@gmail.com>
2024-12-06 11:03:05 -0300

    :wrench: chore: project setup

---
+++ src/main/java/com/VagaPro/vaga_pro/VagaProApplication.java
@@ -0,0 +1,13 @@
+package com.VagaPro.vaga_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VagaProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VagaProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/VagaPro/vaga_pro/VagaProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.VagaPro.vaga_pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VagaProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
