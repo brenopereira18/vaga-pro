ecb962313f4817035c7c649d427d68922a2231b6
brenopereira18 <brenopereira@gmail.com>
2024-12-09 20:59:36 -0300

    :sparkles: feat: add annotation

--- src/main/java/com/VagaPro/vaga_pro/VagaProApplication.java
+++ src/main/java/com/VagaPro/vaga_pro/VagaProApplication.java
@@ -2,8 +2,10 @@ package com.VagaPro.vaga_pro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.VagaPro.vaga_pro")
 public class VagaProApplication {
 
 	public static void main(String[] args) {
