f1046846299edfd607d9a245c6ee5b9955d41d8a
brenopereira18 <brenopereira@gmail.com>
2024-12-16 11:56:33 -0300

    :recycle: refactor: add new annotations

--- src/main/java/com/VagaPro/vaga_pro/modules/company/entities/JobEntity.java
+++ src/main/java/com/VagaPro/vaga_pro/modules/company/entities/JobEntity.java
@@ -2,7 +2,10 @@ package com.VagaPro.vaga_pro.modules.company.entities;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
@@ -10,6 +13,9 @@ import java.util.UUID;
 
 @Entity(name = "job")
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class JobEntity {
 
     @Id
@@ -18,7 +24,7 @@ public class JobEntity {
     private String description;
     private String benefits;
 
-    @NotBlank(message = "Este campo é obrigatório.")
+    @NotBlank(message = "Esse campo é obrigatório.")
     private String level;
 
     @ManyToOne()
