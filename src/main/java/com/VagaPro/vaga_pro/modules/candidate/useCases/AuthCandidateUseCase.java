e3a1f7a8d201e0b60e9cc7ef998a6c5f3405fd35
brenopereira18 <brenopereira@gmail.com>
2024-12-16 15:33:09 -0300

    :sparkles: feat: create token for candidate if he exists

---
+++ src/main/java/com/VagaPro/vaga_pro/modules/candidate/useCases/AuthCandidateUseCase.java
@@ -0,0 +1,57 @@
+package com.VagaPro.vaga_pro.modules.candidate.useCases;
+
+
+import com.VagaPro.vaga_pro.modules.candidate.CandidateRepository;
+import com.VagaPro.vaga_pro.modules.candidate.dto.AuthCandidateDTO;
+import com.VagaPro.vaga_pro.modules.candidate.dto.AuthCandidateResponseDTO;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.naming.AuthenticationException;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
+
+@Service
+public class AuthCandidateUseCase {
+
+    @Value("${security.token.secret.candidate}")
+    private String secretKey;
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public AuthCandidateResponseDTO execute(AuthCandidateDTO authCandidateDTO) throws AuthenticationException {
+        var candidate = this.candidateRepository.findByUsername(authCandidateDTO.username()).orElseThrow(() -> {
+                    throw new UsernameNotFoundException("Username/password incorrect");
+                }
+        );
+        var passwordMatches = this.passwordEncoder.matches(authCandidateDTO.password(), candidate.getPassword());
+
+        if (!passwordMatches) {
+            throw new AuthenticationException();
+        }
+
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+        var expiresIn = Instant.now().plus(Duration.ofMinutes(10));
+        var token = JWT.create().withIssuer("vaga-pro")
+                .withSubject(candidate.getId().toString())
+                .withClaim("roles", Arrays.asList("candidate"))
+                .withExpiresAt(expiresIn)
+                .sign(algorithm);
+
+        var authCandidateResponse = AuthCandidateResponseDTO.builder()
+                .access_token(token)
+                .expires_in(expiresIn.toEpochMilli())
+                .build();
+        return authCandidateResponse;
+    }
+}
