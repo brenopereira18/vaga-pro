4bd555d2c6ae7cbe366abd426a46599156ce0ef2
brenopereira18 <brenopereira@gmail.com>
2025-01-29 11:05:15 -0300

    :sparkles: feat: configure swagger

--- src/main/java/com/VagaPro/vaga_pro/VagaProApplication.java
+++ src/main/java/com/VagaPro/vaga_pro/VagaProApplication.java
@@ -1,15 +1,17 @@
 package com.VagaPro.vaga_pro;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.VagaPro.vaga_pro")
+@OpenAPIDefinition(info = @Info(title = "Gestão de Vagas", description = "API responsável pela gestão de vagas", version = "1"))
 public class VagaProApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VagaProApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(VagaProApplication.class, args);
+    }
 }
