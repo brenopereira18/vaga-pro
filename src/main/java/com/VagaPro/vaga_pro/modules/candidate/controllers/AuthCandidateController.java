4454a9f70af4dfd012abcae47c27afa7c404f4d7
brenopereira18 <brenopereira@gmail.com>
2025-01-30 13:13:42 -0300

    :sparkles: feat: document the url of the methods

--- src/main/java/com/VagaPro/vaga_pro/modules/candidate/controllers/AuthCandidateController.java
+++ src/main/java/com/VagaPro/vaga_pro/modules/candidate/controllers/AuthCandidateController.java
@@ -1,8 +1,14 @@
 package com.VagaPro.vaga_pro.modules.candidate.controllers;
 
 import com.VagaPro.vaga_pro.modules.candidate.dto.AuthCandidateDTO;
-import com.VagaPro.vaga_pro.modules.candidate.dto.AuthCandidateResponseDTO;
 import com.VagaPro.vaga_pro.modules.candidate.useCases.AuthCandidateUseCase;
+import com.VagaPro.vaga_pro.providers.JWTCandidateProvider;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,12 +19,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/candidate")
+@Tag(name = "Candidato", description = "Informações do candidato")
+
 public class AuthCandidateController {
 
     @Autowired
     private AuthCandidateUseCase authCandidateUseCase;
 
     @PostMapping("/auth")
+    @Operation(summary = "Autenticação de candidato", description = "Essa função é responsavel pela autenticação do candidato")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(schema = @Schema(implementation = JWTCandidateProvider.class))
+            }),
+            @ApiResponse(responseCode = "400", description = "Usuário não cadastrado")
+    })
     public ResponseEntity<Object> auth(@RequestBody AuthCandidateDTO authCandidateDTO) {
         try {
             var token = authCandidateUseCase.execute(authCandidateDTO);
